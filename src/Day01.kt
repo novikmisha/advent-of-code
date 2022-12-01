da1b13068902c77a822dd62f142382883c1df27a
novikmisha <novik.misha.1@gmail.com>
2022-12-01 09:51:11 +0300

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,62 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0;
+        var currentGnomeCalories = 0;
+
+        for (calories in input) {
+            if (calories.isEmpty()) {
+                if (currentGnomeCalories > maxCalories) {
+                    maxCalories = currentGnomeCalories
+                }
+
+                currentGnomeCalories = 0
+                continue
+            } else {
+                currentGnomeCalories += calories.toInt();
+            }
+        }
+
+        if (currentGnomeCalories > maxCalories) {
+            maxCalories = currentGnomeCalories
+        }
+
+        return maxCalories;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val topGnomesNumber = 3;
+
+        var topGnomes = IntArray(topGnomesNumber);
+        var currentGnomeCalories = 0;
+
+        for (calories in input) {
+            if (calories.isEmpty()) {
+                topGnomes.forEachIndexed { index, value ->
+                    if (value < currentGnomeCalories) {
+                        topGnomes[index] = currentGnomeCalories;
+                        currentGnomeCalories = value;
+                    }
+                }
+                currentGnomeCalories = 0
+                continue
+            } else {
+                currentGnomeCalories += calories.toInt();
+            }
+        }
+
+        topGnomes.forEachIndexed { index, value ->
+            if (value < currentGnomeCalories) {
+                topGnomes[index] = currentGnomeCalories;
+                currentGnomeCalories = value;
+            }
+        }
+
+        return topGnomes.sum();
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
