6d1f4fa1b1b68860fc404eb3a49919719410fa22
novikmisha <novik.misha.1@gmail.com>
2022-12-26 13:58:31 +0300

    refactor day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,6 +13,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readGroupedInput("Day01_test")
     println(part1(testInput))
+    println(part2(testInput))
 
     val input = readGroupedInput("Day01")
     println(part1(input))
