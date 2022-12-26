2a59e57cc47466147e500118fae140df0e1bc1ed
novikmisha <novik.misha.1@gmail.com>
2022-12-26 14:02:00 +0300

    refactor day 4 solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,33 +1,30 @@
 fun main() {
     fun toRanges(it: String) = it.split(",")
         .map { range ->
-            val splitRange = range.split("-")
-            val start = splitRange[0].toInt()
-            val end = splitRange[1].toInt()
+            val (start, end) = range.split("-").map(String::toInt)
             (start..end).toList()
         }
 
 
     fun part1(input: List<String>): Int {
         return input.map(::toRanges).count {
-            val first = it[0]
-            val second = it[1]
+            val (first, second) = it
             val minSize = minOf(first.size, second.size)
-            first.intersect(second).size == minSize
+            first.intersect(second.toSet()).size == minSize
         }
     }
 
     fun part2(input: List<String>): Int {
         return input.map(::toRanges).count {
-            val first = it[0]
-            val second = it[1]
-            first.intersect(second).isNotEmpty()
+            val (first, second) = it
+            first.intersect(second.toSet()).isNotEmpty()
         }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day04")
     println(part1(input))
