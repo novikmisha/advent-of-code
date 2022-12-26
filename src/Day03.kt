9f24c96810dea2e2dc799b6ba19a6914d77676b2
novikmisha <novik.misha.1@gmail.com>
2022-12-26 13:58:58 +0300

    refactor day 3 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -5,38 +5,35 @@ fun main() {
             .reduce(Set<Char>::intersect)
     }
 
+    fun getCharPriority(it: Char): Int {
+        var result = (it - 'a') + 1
+        if (result < 0) {
+            result += 58
+        }
+        return result
+    }
+
     fun part1(input: List<String>): Int {
-        return input.map {
+        return input.sumOf {
             val chunked = it.chunked(it.length / 2)
 
             val chars = findCommonChar(chunked)
 
-            chars.map {
-                var result = (it - 'a') + 1
-                if (result < 0) {
-                    result += 58
-                }
-                result
-            }.sum()
-        }.sum()
+            chars.map(::getCharPriority).sum()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.chunked(3).map {
+        return input.chunked(3).sumOf {
             val chars = findCommonChar(it)
-            chars.map {
-                var result = (it - 'a') + 1
-                if (result < 0) {
-                    result += 58
-                }
-                result
-            }.sum()
-        }.sum()
+            chars.map(::getCharPriority).sum()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day03")
     println(part1(input))
