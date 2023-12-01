10fe8b8a63a5c283f0b5e9c7c07fe723807b26f4
Mikhail Novik <novik.misha.1@gmail.com>
2023-12-01 08:51:21 +0300

    day 01

---
+++ src/main/kotlin/ListExtension.kt
@@ -0,0 +1,7 @@
+inline fun <T> List<T>.indexOfFirstOrNull(predicate: (T) -> Boolean): Int? {
+    for ((index, item) in this.withIndex()) {
+        if (predicate(item))
+            return index
+    }
+    return null
+}
--- src/main/kotlin/_2023/Day01.kt
+++ src/main/kotlin/_2023/Day01.kt
@@ -2,23 +2,36 @@ package _2023
 
 import Day
 import InputReader
+import indexOfFirstOrNull
 
-class Day01: Day(2023, 1) {
+class Day01 : Day(2023, 1) {
 
-    override val firstTestAnswer = 1
-    override val secondTestAnswer = 2
+    private val numbers = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    )
 
-    override fun first(input: InputReader): Any {
-        println(input.asLine())
-        return 1
-    }
+    override val firstTestAnswer = 142
+    override val secondTestAnswer = 281
 
-    override fun second(input: InputReader): Any {
-        println(input.asLine())
-        return 2
-    }
+    override fun first(input: InputReader) = input.asLines().sumOf { line ->
+            line.firstNotNullOf { char -> char.digitToIntOrNull() } * 10 +
+                    line.reversed().firstNotNullOf { char -> char.digitToIntOrNull() }
+        }
+
+    override fun second(input: InputReader) = input.asLines().sumOf {
+            val sequence = it.windowedSequence(
+                size = numbers.maxOf { number -> number.length }, partialWindows = true
+            ).mapNotNull { window ->
+                window.first().digitToIntOrNull()?.let { int -> return@mapNotNull int }
+
+                numbers.indexOfFirstOrNull { entry ->
+                    window.startsWith(entry)
+                }?.inc()
+            }
+            sequence.first() * 10 + sequence.last()
+        }
 }
 
 fun main() {
-    Day01().solve()
+    Day01().solve(skipTest = true)
 }
