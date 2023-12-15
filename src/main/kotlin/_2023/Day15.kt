b64d23398ff93c4d310da33f7cac56416936c7fc
Mikhail Novik <novik.misha.1@gmail.com>
2023-12-15 11:44:05 +0300

    day 15

--- src/main/kotlin/_2023/Day15.kt
+++ src/main/kotlin/_2023/Day15.kt
@@ -5,13 +5,43 @@ import InputReader
 
 class Day15 : Day(2023, 15) {
 
-    override val firstTestAnswer = 0
-    override val secondTestAnswer = 0
+    override val firstTestAnswer = 1320L
+    override val secondTestAnswer = 145
 
 
-    override fun first(input: InputReader) = 0
+    override fun first(input: InputReader) = input.asLine().split(",").sumOf {
+        it.fold(0L) { result, char ->
+            val ascii = char.code
+            ((result + ascii) * 17) % 256
+        }
+    }
 
-    override fun second(input: InputReader) = 0
+    override fun second(input: InputReader): Int {
+        val lines = input.asLine().split(",")
+        val boxes = mutableMapOf<Int, LinkedHashMap<String, Int>>()
+        lines.forEach {
+            if (it.contains("=")) {
+                val label = it.dropLast(2)
+
+                val hash = label.fold(0) { result, char ->
+                    val ascii = char.code
+                    ((result + ascii) * 17) % 256
+                }
+
+                boxes.getOrPut(hash) { linkedMapOf() }[label] = it.last().digitToInt()
+            } else {
+                val label = it.dropLast(1)
+
+                val hash = label.fold(0) { result, char ->
+                    val ascii = char.code
+                    ((result + ascii) * 17) % 256
+                }
+                boxes.getOrPut(hash) { linkedMapOf() }.remove(label)
+            }
+        }
+        return boxes.entries.filter { it.value.isNotEmpty() }
+            .sumOf { (it.key + 1) * it.value.values.mapIndexed { index, lens -> (index + 1) * lens }.sum() }
+    }
 }
 
 fun main() {
